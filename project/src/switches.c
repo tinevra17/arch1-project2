45a915bafb8eec323dfb07fdae452ca502aafc02
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-27 23:00:11 -0600

    big changes

--- project/src/switches.c
+++ project/src/switches.c
@@ -1,44 +1,39 @@
 #include <msp430.h>
 #include "switches.h"
-// #include "buzzer.h"
+#include "buzzer.h"
+
 
 char switch_state_down, switch_state_changed; /* effectively boolean */
 
-static char 
+static char
 switch_update_interrupt_sense()
 {
-  char p1val = P1IN;
+  char p2val = P2IN;
   /* update switch interrupt to detect changes from current buttons */
-  P1IES |= (p1val & SWITCHES);	/* if switch up, sense down */
-  P1IES &= (p1val | ~SWITCHES);	/* if switch down, sense up */
-  return p1val;
-}
-
-void 
-switch_init()			/* setup switch */
-{  
-  P1REN |= SWITCHES;		/* enables resistors for switches */
-  P1IE |= SWITCHES;		/* enable interrupts from switches */
-  P1OUT |= SWITCHES;		/* pull-ups for switches */
-  P1DIR &= ~SWITCHES;		/* set switches' bits for input */
-  switch_update_interrupt_sense();
-  led_update();
+  P2IES |= (p2val & SWITCHES);/* if switch up, sense down */
+  P2IES &= (p2val | ~SWITCHES);/* if switch down, sense up */
+  return p2val;
 }
 
 void
-switch_interrupt_handler()
+switch_init()/* setup switch */
 {
-  char p1val = switch_update_interrupt_sense();
+  P2REN |= SWITCHES;/* enables resistors for switches */
+  P2IE |= SWITCHES;/* enable interrupts from switches */
+  P2OUT |= SWITCHES;/* pull-ups for switches */
+  P2DIR &= ~SWITCHES;/* set switches' bits for input */
+  switch_update_interrupt_sense();
+  switch_interrupt_handler();
+}
 
-  char button1 = (p1val & SW2) ? 0: 1;
-  //switch_state_down = (p1val & SW1) ? 0 : 1; /* 0 when SW1 is up */
+void switch_interrupt_handler(){
+  char p2val = switch_update_interrupt_sense();
+  char button1 = (p2val & SW1) ? 0 : SW1;
+  char button2 = (p2val & SW2) ? 0 : SW2;
+  char button3 = (p2val & SW3) ? 0 : SW3;
+  char button4 = (p2val & SW4) ? 0 : SW4;
 
   if(button1){
-    buzzer_init();
     song();
   }
-
-
-  // switch_state_changed = 1;
-  // led_update();
 }
