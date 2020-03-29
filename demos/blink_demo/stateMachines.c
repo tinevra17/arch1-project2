a27e450280df5511aec8e56fcbf1457af1784549
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-28 19:39:53 -0600

    led test

--- demos/blink_demo/stateMachines.c
+++ demos/blink_demo/stateMachines.c
@@ -16,7 +16,7 @@ sm_slow_clock()		/* slowly cycle through {off, dim, bright} */
 void
 sm_fast_clock()	/* quickly cycle through 0...3 */
 {
-  pwmCount = (pwmCount + 1) & 3; 
+  pwmCount = (pwmCount + 1) ; 
 }
 
 void 
@@ -25,14 +25,12 @@ sm_update_led()
   char greenB;
   switch (cases) {
   case one:
-    red_on = (pwmCount < 1);
     greenB = 0; break; //off
-   
   case two:
     greenB = (pwmCount < 5); break; //bright
   case three:
-   red_on = 1;
-    greenB = (pwmCount < 1); break; 
+    red_on = 1;
+    greenB = (pwmCount < 1); break;  //dim
   }
 
   if (green_on != greenB) {
