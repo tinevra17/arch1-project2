2a11d1663d73caa968785291875b23b4677205bd
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-28 22:38:04 -0600

    changes

--- project/src/buttonMain.c
+++ project/src/buttonMain.c
@@ -9,8 +9,8 @@ void main(void)
   configureClocks();
   switch_init();
   buzzer_init();
-  led_init();       
-  enableWDTInterrupts();    
+    
+      
   // enableWDTInterrupts();
 
   or_sr(0x18);  // CPU off, GIE on
