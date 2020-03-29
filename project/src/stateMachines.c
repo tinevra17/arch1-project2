819fa8e2f4b33b6114707bbf07a306fa93a92e87
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-29 00:40:09 -0600

    changes

--- project/src/stateMachines.c
+++ project/src/stateMachines.c
@@ -4,35 +4,38 @@
 
 
 // state variables 
-static enum {off=0, on=1, dim=2, } cases;
+static enum {off=0, on=1, dim=2 } cases;
 static char count = 0;		
 
-void
-caseSwitcher()	
+void caseSwitcher()	
 {
   cases = (cases + 1) % 3;
 }
 
-void
-fast()	/* quickly cycle through 0...3 */
+void fast()
 {
-  count = (count + 1) % 3; 
+  if(count == 3){
+    count = 0;
+  }
+  count++
+
+ // count = (count + 1) % 3; 
 }
 
 void ledStates()
 {
-  char greenB;
+  char temp;
   switch (cases) {
   case off:
-    greenB = 0; break; //off
+    temp = 0; break; //off
   case on:
-    greenB = 1; break; //on
+    temp = 1; break; //on
   case dim:
-    greenB = (count < 1); break;  //dim
+    temp = (count < 1); break;  //dim
   }
 
-  if (green_on != greenB) {
-    green_on = greenB;
+  if (green_on != temp) {
+    green_on = temp;
     led_changed = 1;
   }
 }
