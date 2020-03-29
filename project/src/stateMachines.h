b29ca34dfcb927c3b0d83cb127b244b69f36cf73
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-29 00:35:22 -0600

    changes

--- project/src/stateMachines.h
+++ project/src/stateMachines.h
@@ -3,13 +3,7 @@
 #ifndef stateMachine_included
 #define stateMachine_included
 
-// void state_advance();
-// char toggle_red();
-// char toggle_green();
-// char toggle_red_green();
-
-
-
 void redLightOff();
 void redLightOn();
+
 #endif // included
