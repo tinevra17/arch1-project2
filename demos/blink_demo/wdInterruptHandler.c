669103ac21cf5c5aace60e186f3bb576de61d787
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-28 20:07:15 -0600

    led test

--- demos/blink_demo/wdInterruptHandler.c
+++ demos/blink_demo/wdInterruptHandler.c
@@ -5,10 +5,10 @@ void
 __interrupt_vec(WDT_VECTOR) WDT(){	/* 250 interrupts/sec */
   static char blink_count = 0;
   if (++blink_count == 125) {
-    fast(); 
-    //caseSwitcher();
+   
+    caseSwitcher();
   }
-  // fast(); //makes led blink
-  // ledStates();
-  // led_update();
+  fast(); //makes led blink
+  //ledStates();
+  led_update();
 }
