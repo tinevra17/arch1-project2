53a6b3dc749e61f4a44ece38223ff1e50040d2f2
Samuel Tinevra <tinevra17@hotmail.com>
2020-03-28 21:00:54 -0600

    led test

--- project/src/led.c
+++ project/src/led.c
@@ -1,7 +1,7 @@
 #include <msp430.h>
 #include "led.h"
 
-unsigned char red_on = 0, green_on = 0;
+unsigned char red_on = 0, green_on = 0, led_state=0;
 unsigned char led_changed = 0;
 static char redVal[] = {0, LED_RED}, greenVal[] = {0, LED_GREEN};
 
